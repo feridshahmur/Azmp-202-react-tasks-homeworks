f3d815870aa7d3e68bb985026d691f3a4db9b979
vusalasalimovaa <salimovavusala7@gmail.com>
2024-12-16 14:10:37 +0400

    first react task done

--- 01.react intro/src/App.jsx
+++ 01.react intro/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Card
           src="https://mcdonalds.az/images/eec170dd5bc3cab3f86f9fabf4531f29.png"
           title="Chicken Wings"
-          desc="Bol baharatlı əsl nahar atışdırmalığı,Dadmağa tələs!"
+          desc="Bol baharatlı əsl nahar atışdırmalığı,dadmağa tələs!"
           price={6.5}
 
         />
